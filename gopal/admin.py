07edf83634332148021b414ded024313e21df5eb
Sajad <sajadkhosravani1@gmail.com>
2020-09-05 22:44:44 +0430

    admin has been configured for new models.

--- gopal/admin.py
+++ gopal/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Ownership)
+class OwnershipAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Media)
+class MediaAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(House)
+class HouseAdmin(admin.ModelAdmin):
+    pass
