7ed85893a2d868d8e32827656d3269ff07bb8b10
sajadkhosravani1 <sajadkhosravani1@gmail.com>
2020-09-12 01:06:46 +0430

    URLs has been named.

--- gopal/urls.py
+++ gopal/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from gopal.views import *
 
+app_name = 'gopal'
+
 urlpatterns = [
-    path('',home),
-    path('list', list),
-    path('estimate',estimate),
-    path('R<int:house_id>', house_view)
+    path('', home, name='home'),
+    path('list', list, name='list'),
+    path('estimate', estimate, name='estimate'),
+    path('R<int:house_id>', house_view, name='house_view')
 ]
