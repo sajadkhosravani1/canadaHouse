abf7e933f20f0ce833c2ec951c98824e9238fd92
sajadkhosravani1 <sajadkhosravani1@gmail.com>
2020-09-14 05:02:42 +0430

    Estimate view is ready to use.

--- gopal/views.py
+++ gopal/views.py
@@ -15,18 +15,13 @@ def list(request):
     empty = False
 
     if hasattr(request,'GET'):
-        print(request.GET)
         if len(houses)>0 and 'price_least' in request.GET.keys() and len(request.GET['price_least']):
-            print('price_least:',request.GET['price_least'])
             houses = houses.filter(price__gt=int(request.GET['price_least']))
         if len(houses)>0 and 'price_most' in request.GET.keys() and len(request.GET['price_most']):
-            print('price_most:',request.GET['price_most'] )
             houses = houses.filter(price__lt=int(request.GET['price_most']))
         if len(houses)>0 and 'city' in request.GET.keys() and len(request.GET['city']):
-            print('city:',request.GET['city'])
             houses = houses.filter(city=City.objects.get(name=request.GET['city']))
         if len(houses)>0 and 'state' in request.GET.keys() and len(request.GET['state']):
-            print('state:',request.GET['state'])
             houses = houses.filter(state=State.objects.get(name=request.GET['state']))
 
     if len(houses) == 0: empty = True
@@ -48,7 +43,28 @@ def list(request):
 
 
 def estimate(request):
-    context = {}
+    context = dict()
+    context["states"] = [state.name for state in State.objects.all()]
+    context["cities"] = [city.name for city in City.objects.all()]
+    context['types'] = [type.name for type in Type.objects.all()]
+    context['ownerships'] = [item.value for item in Ownership.objects.all()]
+
+    if request.method=="POST":
+
+        for key, value in request.POST.items():
+            print(key,value)
+            context[key] = value
+        del context['csrfmiddlewaretoken']
+
+        with open('houses_trained.pkl', 'rb') as f:
+            import pickle
+            regr = pickle.load(f)
+            predicted = regr.predict([
+                [int(context['bedrooms']), int(context['baths']), int(context['size']),
+                Type.objects.get(name=context['type']).id, State.objects.get(name=context['state']).id,
+                City.objects.get(name=context['city']).id, Ownership.objects.get(value=context['ownership']).id]])
+            context['predicted_price'] = int(predicted[0])
+
     return render(request,'gopal/estimate.html',context)
     pass
 
